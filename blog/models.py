f1e92de1cba5c17bf230372c2cf87f7eab8ccf9f
tangoc2712 <taquangngoc.hh3@gmail.com>
2022-01-12 16:02:46 +0700

    Add model, view, url

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status="published")
+
+
+class Post(models.Model):
+    STATUS_CHOICE = (
+        ("draft", "Draft"),
+        ("published", "Published"),
+    )
+
+    title = models.CharField(max_length=250)
+
+    # for SEO URLs
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post")
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICE, default="draft")
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
+
+    class Meta:
+        ordering = ("-publish",)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )
