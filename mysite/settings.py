244a5ec5b96ce69bf8e31470d9ba91ea4e51ec9c
tangoc2712 <taquangngoc.hh3@gmail.com>
2022-02-28 06:42:22 +0700

    alo

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "ckeditor",
     "blog.apps.BlogConfig",
-    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
@@ -134,10 +133,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-import dj_database_url
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES["default"].update(db_from_env)
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
